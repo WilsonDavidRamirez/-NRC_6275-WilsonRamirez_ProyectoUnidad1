f9320a8895b2dbb07ad2058df25ab2e91ed326e4
Wilson R <wdramirez@espe.edu.ec>
2022-06-07 17:57:40 -0500

    Primero

--- index.py
+++ index.py
@@ -8,21 +8,8 @@ tareasPendientes = []
 @app.route('/' )
 
 def principal():
-    return render_template('pagina1.html', tareasLista = tareasPendientes)
+    return render_template('pagina1.html')
 
-
-@app.route('/enviar',  methods=['GET','POST'])
-
-def enviar():
-    if(request.method == "POST"):
-        tareas = request.form['tarea']
-        correos = request.form['correo']
-        prioridades = request.form['prioridad']
-        tareasPendientes.append({'tarea': tareas, 'correo': correos, 'prioridad': prioridades })
-        return redirect(url_for('principal'))
-
-
-# main del programa 
 if __name__ == '__main__':
-    #debug = true, para reiniciar automaticamente el servidor, tiempo de desarrollo 
+    
     app.run(debug=True)
